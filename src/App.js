e472a16777cc2b8d4e9b6f0043dd2f2fd4d1db05
unknown <amandalragone@gmail.com>
2019-06-12 21:26:06 -0500

    Added static elements.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import ImageCard from './components/ImageCards';
 import './App.css';
 import images from './images.json';
-
+import Navbar from './components/Navbar';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
 
@@ -12,7 +15,9 @@ class App extends Component {
 
   render() {
     return (
-      
+      <Wrapper>
+      <Navbar></Navbar>
+      <Header></Header>
       <div className="container">
         <div className="row">
           {this.state.images.map(image => (
@@ -24,7 +29,8 @@ class App extends Component {
           ))}
         </div>
       </div>
-      
+      <Footer></Footer>
+      </Wrapper>
 
     );
   
