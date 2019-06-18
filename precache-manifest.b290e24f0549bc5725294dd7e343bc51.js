090b2cae8935d1047bd5e11d3fd19a49bded4314
Amanda <amandalragone@gmail.com>
2019-06-17 22:07:30 -0500

    Updates

---
+++ precache-manifest.b290e24f0549bc5725294dd7e343bc51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c92b8a78757d08badb8077ffb9fc059",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "ff68ae06ea66cda985a9",
+    "url": "/clicky-game/static/css/main.9adbf3f9.chunk.css"
+  },
+  {
+    "revision": "b8f92aaec45499073dfc",
+    "url": "/clicky-game/static/js/2.813d4645.chunk.js"
+  },
+  {
+    "revision": "ff68ae06ea66cda985a9",
+    "url": "/clicky-game/static/js/main.9278c678.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
